0e2023ddb796f8655be948d84c4cbdbb7a2aa0ef
John Reese <agglerithm@gmail.com>
2020-05-09 16:45:31 -0500

    Added token type, rearranged hierarchy

---
+++ CJR.PDF.Parser.Domain/TokenType.cs
@@ -0,0 +1,8 @@
+﻿namespace CJR.PDF.Read.Domain
+{
+    public enum TokenType
+    {
+        Comment, BooleanObject, IntObject, RealObject, StringObject, NameObject, DictionaryObject, ArrayObject, 
+        StreamObject, NullObject, 
+    }
+}
